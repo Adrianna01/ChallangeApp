6e58451c9eadeda66f72f23c84ecbba7d824e949
Ada <Ada@LAPTOP-KKCLKJ24>
2023-01-31 18:17:29 +0100

    Zadanie 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+var name = "Ewa";
+int age = 33;
+bool isFemale = true;
+
+if (isFemale && age < 30) ;
+{
+    Console.WriteLine(" KOBIETA PONIZEJ 30 LAT");
+}
+if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine(" EWA, LAT 33");
+}
+if (!isFemale && age < 18)
+{
+    Console.WriteLine(" NIEPELNOLETNI MEZCZYZNA");
+}
